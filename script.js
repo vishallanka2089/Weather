53ea1ad08b00dec99ce04770329b9bcf64ae2124
vishallanka2089 <104144841+vishallanka2089@users.noreply.github.com>
2022-11-14 07:51:29 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+//Fetching Current Location when page loads
+window.addEventListener("load", ()=> {
+    let long;
+    let lat;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            console.log(position);
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=723790ade36d14263fd8423118f5987f`;
+            console.log(api);
+            fetch(api)
+                .then(response => response.json())
+                .then(data => {
+                    let city = data.name;
+                    console.log(city)
+                    weather.fetchWeather(`${city}`)
+  
+
+                });
+            
+        });
+    }
+});
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+//Fetching Weather Function
+
+let weather = {
+    apiKey: "723790ade36d14263fd8423118f5987f",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      document.querySelector(".city").innerText = "Weather in " + name;
+      document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText = description;
+      var numb= ((temp*(9/5))+32);
+      var rounded = Math.round((numb + Number.EPSILON) * 100) / 100;
+      document.querySelector(".temp").innerText = temp + "°C / "+ (rounded)+ "°F";
+      document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Wind speed: " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+      
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+  
+  document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+
+
+
+function searchToggle(obj, evt){
+        var container = $(obj).closest('.search-wrapper');
+            if(!container.hasClass('active')){
+                container.addClass('active');
+                evt.preventDefault();
+            }
+            else if(container.hasClass('active') && $(obj).closest('.input-holder').length == 0){
+                container.removeClass('active');
+                // clear input
+                container.find('.search-input').val('');
+            }
+    }
